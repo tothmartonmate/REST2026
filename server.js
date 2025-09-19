b814342878fe3e4214a85de21ea4f8098fe688c2
Loyder6 <fmatevok@gmail.com>
2025-09-19 11:18:49 +0200

    elod

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express"); //Express importálása
+const app = express(); //Express példányosítása
+const port = 3000; //Port beállítása
+
+//Middleware - köztes alkalmazások
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+]
+
+
+//GET végpont egy szöveges üzenet visszaküldésre
+app.get('/hello', (req, res) => {
+    res.send("Hello itt az Express webszerver!");
+})
+
+app.get('/api/courses', (req, res) => {
+    res.json(courses);
+})
+
+app.get('/api/courses', (req, res)=>{
+    //Keresés a tömbben
+    const course = find(course.id === parseInt(req.params.id));
+})
+
+
+//A webszerver elindítása
+app.listen(port, () => {
+    console.log(`A webszerver figyel a localhost:${port} webcímen`);
+})
