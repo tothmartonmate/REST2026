27bc26882d6264076e588a882c7096fd02fb0878
Loyder6 <fmatevok@gmail.com>
2025-09-24 10:30:56 +0200

    Post es delete vegpontok

--- server.js
+++ server.js
@@ -17,25 +17,38 @@ app.get('/hello', (req, res) => {
     res.send("Hello itt az Express webszerver!");
 })
 
-app.get('/api/courses', (req, res) => {
+app.get('/api.courses', (req, res) => {
     res.json(courses);
 })
 
-app.get('/api/courses/:id', (req, res)=> {
+app.get('/api.courses/:id', (req, res)=> {
     //Keresés a tömbben
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('A megadott id-val nem létezik kurzus!');  res.json(course);
 })
 
+app.get('/api.courses/:id', (req, res)=> {
+    //Keresés a tömbben
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('A megadott id-val nem létezik kurzus!');  res.json(course);
+})
 
-app.post('/api/courses', (req, res) => {
+app.post('/api.courses', (req, res) => {
     const course = {
         id: courses.length +1,
         name: req.body.name
     }
 
     courses.push(course);
-    res.json(req.body);
+    res.status(200).json({mesage: "Új kurzus hozzáadva!", data: req.body});
+})
+
+app.delete('/api.courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('A megadott id-val nem létezik kurzus!');
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.status(200).json({message: "Kurzus törölve!", data: course});
 })
 
 //A webszerver elindítása
